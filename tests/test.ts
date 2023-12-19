bb438624658edd485066fe1b666594a26d48e6bf
Jeffrey Mostroso <1376038-jeffrey-omega@users.noreply.gitlab.com>
2023-12-19 22:26:34 +0000

    Add test for index page and generate screenshot

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,17 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
+
+test.describe('index page', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto('/');
+	});
+
+	test('has expected h1', async ({ page }) => {
+		await page.waitForLoadState('domcontentloaded');
+		
+		await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+		// generate a screenshot of the page
+		await page.screenshot({ path: 'static/screenshot.png', fullPage: true });
+	});
+});
